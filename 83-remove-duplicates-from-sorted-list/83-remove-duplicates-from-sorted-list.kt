892375646986c9a070c566da778999830d10b1b7
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-01-12 14:04:28 +0900

    Time: 342 ms (5.21%), Space: 39.9 MB (6.25%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.kt
@@ -0,0 +1,20 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        var node = head
+        while(node != null) {
+            if(node.next != null && node.`val` == node.next.`val`)
+                node.next = node.next.next
+            else node = node.next
+        }
+        return head
+    }
+}
